e8b6f9a4ff2b82f81c85287c5a2c15935841d327
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-04 23:19:37 +0200

    Update bit++.py

--- bit++.py
+++ bit++.py
@@ -1,14 +1,3 @@
-""" Enunciado 
-Em Bitland, a linguagem de programação Bit++ é peculiar e complicada. 
-Ela possui uma única variável, "x", e duas operações: "++" que aumenta o valor de "x" em 1, e "--" que diminui o valor de "x" em 1. 
-Um programa Bit++ é uma sequência de instruções que devem ser executadas em ordem.
-
-Input:
-> Um inteiro "n" (1 ≤ n ≤ 150) - o número de instruções no programa.
-> "n" linhas contendo uma instrução cada. Cada instrução contém uma operação ("++" ou "--") e a variável "x", sem espaços.
-Output:
-> Um único número inteiro - o valor final de "x" após a execução do programa. """
-
 num_instrucoes = int(input())
 instrucoestotal = [] # Se cria uma lista para que cada instrução possa ser interpretada individualmente
 
