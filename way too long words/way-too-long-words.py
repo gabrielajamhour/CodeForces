ae18ec7a44893de7278f486e135ba5552a0312c8
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-04 23:34:51 +0200

    Update way-too-long-words.py

--- way too long words/way-too-long-words.py
+++ way too long words/way-too-long-words.py
@@ -1,13 +1,3 @@
-""" Enunciado
-Em um texto, palavras com mais de 10 caracteres são substituídas por abreviações.
-A abreviação consiste na primeira e última letra da palavra, entre as quais é escrito o número de letras entre elas.
-
-> Input:
-    Um inteiro n (1 ≤ n ≤ 100).
-    n palavras compostas por letras minúsculas, com comprimentos entre 1 e 100 caracteres.
-> Output:
-    n linhas contendo as palavras após a substituição por abreviações, se aplicável. """
-
 num_inputs = int(input())
 palavrastotal = [] # Se cria uma lista para que cada palavra possa ser interpretada individualmente
 
