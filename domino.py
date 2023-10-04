69b338fd9acec9d2b035a1a4b9b497ecb8824a46
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-04 23:22:49 +0200

    Update domino.py

--- domino.py
+++ domino.py
@@ -1,15 +1,3 @@
-""" Enunciado 
-Você recebe um tabuleiro retangular de M x N quadrados e um número ilimitado de peças de dominó padrão, cada uma com dois quadrados.
-A tarefa é posicionar essas peças no tabuleiro de forma a cobrir o máximo possível de quadrados, seguindo as regras fornecidas:
-  1. Não há dominós sobrepostos
-  2. Cada peça deve estar inteiramente dentro do tabuleiro
-O desafio é encontrar o número máximo de dominós que podem ser colocados.
-
-Input: 
-> Dois inteiros: M e N (1 ≤ M ≤ N ≤ 16), representando as dimensões de um tabuleiro retangular.
-Output:
-> Número máximo de dominós 2 × 1 que podem ser colocados no tabuleiro, seguindo as condições especificadas. """
-
 M_N = str(input()) # O usuário deve digitar M e N, separados por um espaço
 # Para tratar M e N como variáveis independentes e, tendo em vista que se tratam de números inteiros, transformamos em int
 
