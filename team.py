4ef6cd9c0ae86422d3f7265dae3a755066e40f59
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-04 23:30:52 +0200

    Update team.py

--- team.py
+++ team.py
@@ -1,13 +1,3 @@
-""" Enunciado
-  Três amigas participam de um concurso de programação com vários problemas. Elas resolvem um problema se pelo menos duas delas têm certeza da solução.
-  Dada a confiança delas em cada problema, determine quantos problemas elas resolverão.
-
-> Input:
-    n (1 ≤ n ≤ 1000): número de problemas.
-    Para cada problema, três inteiros (0 ou 1) representando a confiança das três amigas.
-> Output:
-    Um único inteiro: o número de problemas que as amigas resolverão. """
-
 num_problemas = int(input())
 problemastotal = []  # Se cria uma lista para que cada problema possa ser interpretado individualmente
 
