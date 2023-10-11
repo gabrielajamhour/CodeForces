b8ba9bdef8e4a0ed7721f736f2b42f3605fe3db7
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-11 10:38:04 +0200

    Update puzzles_v1.py

--- Difficulty 0900/Puzzles/puzzles_v1.py
+++ Difficulty 0900/Puzzles/puzzles_v1.py
@@ -16,3 +16,7 @@ if students == 2 and len(pieces) != len(set(pieces)):
 else:
     difference = sub_pieces[-1] - sub_pieces[0]    
     print(difference)
+
+'''
+Esse código teria funcionado. No entanto, ele não considera que existe a combinação de 4 números subsequentes dentro da lista em que a diferença entre o primeiro e o quarto seja menor que a diferença entre os 4 menores números da lista.
+'''
