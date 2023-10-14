1756172788db24f7905be26ff74a8d5c4b8b1910
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-14 15:58:54 +0200

    Update nearly-lucky-number-error1.py

--- Difficulty 0800/nearly lucky number/nearly-lucky-number-error1.py
+++ Difficulty 0800/nearly lucky number/nearly-lucky-number-error1.py
@@ -11,3 +11,13 @@ if lucky < 0:
     print("NO")
 else:
     print("YES")
+
+'''
+Erro: 
+input: 7
+output: YES
+answer: NO
+
+Foi dado o output incorreto pois, de acordo com Petya, o número 7 é um "lucky number", mas ele não está em busca de lucky numbers.
+Petya quer números "nearly lucky", ou seja, cuja quantidade de dígitos é um lucky number (4 ou 7).
+'''
