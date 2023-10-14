be5d2169f6060d41b3861959aa207786301ab700
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-14 15:40:14 +0200

    Create elephant.py

---
+++ Difficulty 0800/elephant/elephant.py
@@ -0,0 +1,18 @@
+x = int(input())
+
+passos = 0
+
+while x > 0:
+    if x >= 5:
+        x -= 5
+    elif x >= 4:
+        x -= 4
+    elif x >= 3:
+        x -= 3
+    elif x >= 2:
+        x -= 2
+    else:
+        x -= 1
+    steps += 1
+
+print(passos)
