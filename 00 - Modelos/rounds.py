b872c65c20861636c049dd5ae31482ae9be9da05
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-15 15:31:53 +0200

    Update rounds.py

--- 00 - Modelos/rounds.py
+++ 00 - Modelos/rounds.py
@@ -1,19 +1,17 @@
 test_cases = int(input())
 lista = []
 
-for i in range(test_cases):
-    # Jeito 1 (passo a passo)
-    x = input()
-    x_split = x.split()
-    int_x = [int(num) for num in x_split]
-    lista.append(int_x)
-
-    # Jeito 2 (direto)
-        # A função map() é uma função embutida que permite aplicar uma função a todos os elementos de um ou mais iteráveis, como listas
-
-    # Dois números juntos de string para int separadamente
+for _ in range(test_cases):
+    # A função map() é uma função embutida que permite aplicar uma função a todos os elementos de um ou mais iteráveis, como listas
+    # Dois números separados por um espaço de string para int separadamente
     n, p = map(int, input().split())
 
-    # Vários números juntos de string para int em uma lista 
+    # Vários números separados por espaços de string para int em uma lista 
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
+
+    # Vários valores sem separação em uma lista separados
+    linha = str(input())
+    linha_lista = [valor for valor in linha_string]
+    lista.append(linha_lista)
+
