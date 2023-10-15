b7df2058ccfa1076912472187da81d7863151fb9
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-15 15:40:19 +0200

    Update rounds.py

--- 00 - Modelos/rounds.py
+++ 00 - Modelos/rounds.py
@@ -13,5 +13,9 @@ for _ in range(test_cases):
     # Vários valores sem separação em uma lista separados
     linha = str(input())
     linha_lista = [valor for valor in linha_string]
+    # Ou, para que os valores sejam convertidos para inteiros:
+    linha_lista = [int(valor) for valor in linha_string]
     lista.append(linha_lista)
+
+
 
