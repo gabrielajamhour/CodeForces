59f01b3c0063daa7d0c994d9dd6a5c81bea3e414
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-26 11:13:40 +0200

    Update new3.py

--- 00 - Modelos/new3.py
+++ 00 - Modelos/new3.py
@@ -1,8 +1,5 @@
 # Organiza a lista em ordem crescente
 lista.sort()
 
-# Encontrar o maior valor entre os listados
-maior = max(valor1, valor2, valorn)
-
 # Cópias da lista inicial
 lista_copia = lista[:]
