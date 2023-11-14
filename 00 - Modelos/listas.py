1ac138108f1d80b6f673398957f8ecab089fbdda
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-14 18:37:37 +0100

    Delete 00 - Modelos directory

--- 00 - Modelos/listas.py
+++
@@ -1,5 +0,0 @@
-# Organiza a lista em ordem crescente
-lista.sort()
-
-# Cópias da lista inicial
-lista_copia = lista[:]
--- 00 - Modelos/set.py
+++
@@ -1,5 +0,0 @@
-usuario = str(input())
-caracteres_unicos = set() # A função set() é usada para criar uma coleção não ordenada de elementos únicos > não permite elementos duplicados
-
-for caractere in usuario:
-    caracteres_unicos.add(caractere) # Adiciona apenas elementos únicos > se um elemento já estiver lá, não é adicionado
