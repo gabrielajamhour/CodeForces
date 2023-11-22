aa5daa70a89bf67fd833fe9512747dd6c2ed985d
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-22 11:58:03 +0100

    Create sum.py

---
+++ Modelos/sum.py
@@ -0,0 +1,15 @@
+# Criar uma matriz de exemplo
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Índice da sublista a ser somada
+indice_sublista = 1
+
+# Somar os elementos da sublista
+soma = sum(matriz[indice_sublista])
+
+# Imprimir o resultado
+print(f"A soma dos elementos da sublista {indice_sublista} é: {soma}")
