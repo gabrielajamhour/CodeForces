88169e72dff3d8c97fa3aa3ef17bf78310b671de
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-22 10:06:30 +0100

    Create a.py

---
+++ Modelos/a.py
@@ -0,0 +1,7 @@
+tostadaAntes = [list(map(str, input().split())) for _ in range(lado)]
+
+for _ in range(lado):
+    linea = list(map(str, input().split()))
+    tostadaAntes.append(linea)
+
+# Es lo mismo
